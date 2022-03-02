01945b6b088e34a09e5666cc0543e11a10f33bec
Drgneel <ankursajwan10@gmail.com>
2022-03-02 18:43:48 +0530

    Breaking homepage to smaller reusable components  and styling our page

---
+++ src/components/menu-item/menu-item.components.jsx
@@ -0,0 +1,20 @@
+import "./menu-item.styles.scss";
+
+const MenuItem = ({ id, title, imageUrl, size }) => {
+  return (
+    <div className={`${size} menu-item`}>
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div key={id} className="content">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
+    </div>
+  );
+};
+
+export default MenuItem;

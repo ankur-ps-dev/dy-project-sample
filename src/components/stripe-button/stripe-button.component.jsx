918f105ed7f822035798c2b613b5a44857e5ac90
Drgneel <ankursajwan10@gmail.com>
2022-03-09 11:47:25 +0530

    Integerating Stripe payments and our website title with icon

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51KbIOfSFZMgP2c02ulEX0UNSOuNYIEgiiRAP0jnOXaySG7O0YRqT82jNhgDmYkvAimG5JHzR7zFntsnKpDQpD0xF007XbGG6Pi";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Succesful!");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
--- src/pages/checkout/checkout.components.jsx
+++ src/pages/checkout/checkout.components.jsx
@@ -4,6 +4,7 @@ import {
   selectCartItems,
   selectCartItemsTotal,
 } from "../../redux/cart/cart.selectors";
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
 import "./checkout.styles.scss";
 import CheckoutItem from "../../components/checkout-item/checkout-item.components";
 
@@ -31,6 +32,12 @@ const CheckoutPage = ({ cartItems, total }) => {
         <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       ))}
       <div className="total">TOTAL: ${total}</div>
+      <div className="test-warning">
+        *Please use the following test credit card for payments*
+        <br />
+        4242 4242 4242 4242 - Exp: 01/23 - CVV: 123
+      </div>
+      <StripeCheckoutButton price={total} />
     </div>
   );
 };
